22d555193800fa2525174b911755cca5ccbc0b0f
Ubuntu <labuser@ubuntu2204.42umbujjnisupdiqdiha3c3o3e.rx.internal.cloudapp.net>
2024-07-01 09:33:09 +0000

    adding

--- Portfolio-website/event-handler/src/EventHandling.js
+++ Portfolio-website/event-handler/src/EventHandling.js
@@ -1,5 +1,30 @@
 import React from "react";
 
 class EventHandling extends React.Component{
-    
+   constructor( props ){
+    super(props)
+    this.state = {
+        message : ''
+    };
+    // we want to bind the event handler method to the current object
+    this.handle_click = this.handle_click.bind(this);
+   } 
+   handle_click(){
+    console.log("Handle click");
+    this.setState({
+         message: 'Remember to drink at least  8 glasses of water a day'
+    });
+   }
+   render (){
+    return (
+        <div className="container mt-4"> 
+            <h2>Daily Health Tips</h2>
+            <button className="btn btn-primary" onClick={this.handle_click}> Get Health Tips</button>
+            <p>{this.state.message}</p>
+        </div>
+    );
+   }
 }
+
+export default EventHandling;
+
