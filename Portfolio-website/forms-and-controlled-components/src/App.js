5c6966664b20c5bc3da3797526cb83e3a4b70669
Ubuntu <labuser@ubuntu2204.wcnpqnc1d5te5it14wib31xx3a.rx.internal.cloudapp.net>
2024-07-02 09:50:05 +0000

    Uncontrolled form done

--- Portfolio-website/forms-and-controlled-components/src/App.js
+++ Portfolio-website/forms-and-controlled-components/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ControlledForm from './ControlledForm';
+import UncontrolledForm from './UncontrolledForm';
 
 function App() {
   return (
     <div className="App">
-      <ControlledForm/>
+      {/* <ControlledForm/> */}
+      <UncontrolledForm/>
     </div>
   );
 }
--- Portfolio-website/forms-and-controlled-components/src/UncontrolledForm.js
+++ Portfolio-website/forms-and-controlled-components/src/UncontrolledForm.js
@@ -7,7 +7,39 @@ class UncontrolledForm extends React.Component{
         this.emailInput = React.createRef();
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+
+   handleSubmit( event ){
+    alert("Form submitted: Name "+this.nameInput.current.value +" Email "+this.emailInput.current.value);
+    event.preventDefault();
+   } 
+
+   render(){
+    return(
+        <div className="container mt-4">
+            <h2>Uncontrolled Form</h2>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label>
+                        Name
+                        <input type="text" name="name" className="form-control" ref={this.nameInput}/>
+                    </label>
+                </div>
+                <div className="form-group">
+                    <label>
+                        Email
+                        <input type="email" name="email" className="form-control" ref={this.emailInput}/>
+                    </label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit </button>
+            </form>
+
+        </div>
+    );
+
+   }
+
 
 
 }
+
+export default UncontrolledForm;
