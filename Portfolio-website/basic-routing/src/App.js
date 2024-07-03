9dced86a67f4ef6386fbdbd1e8db31cf6a81f221
Ubuntu <labuser@ubuntu2204.jzx5afjpbpnupisvvtbsub54jd.rx.internal.cloudapp.net>
2024-07-03 12:00:39 +0000

    Basic Routing over

--- Portfolio-website/basic-routing/src/App.js
+++ Portfolio-website/basic-routing/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import React from 'react';
-// import { Route, Switch, Link } from 'react-router-dom';
-import { Route, Routes, Link, Router } from 'react-router-dom';
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
   
 import './App.css';
 import Home from './Home';
@@ -10,22 +9,23 @@ import About from './About';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
     <div className="App">
      <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/About">About</Link></li>
         <li><Link to="/Contact">Contact</Link></li>
+        <p>ho</p>
       </ul>
      </nav>
-    <Routes>
-      <Route exact path = "/" element={<Home/>}/> 
-      <Route exact path = "/About" element={<About/>}/> 
-      <Route exact path = "/Contact" element={<Contact/>}/> 
-    </Routes>
+     <Routes>
+      <Route path = "/" element={<Home/ >}/> 
+      <Route path = "/About" element={<About />}/> 
+      <Route path = "/Contact" element={<Contact />}/> 
+    </Routes> 
     </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
