c586fd2b21adaa59dc66cd918fed9457fb23514b
Ubuntu <labuser@ubuntu2204.jzx5afjpbpnupisvvtbsub54jd.rx.internal.cloudapp.net>
2024-07-03 10:34:16 +0000

    some work

--- Portfolio-website/context-api-example/src/App.js
+++ Portfolio-website/context-api-example/src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import UserProfile from './UserProfile';
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
+import Dashboard from './Dashboard';
 
 function App() {
+  const { user } = useContext(UserContext);
   return (
     <div className="App">
-       <UserProfile/>
+       <Navbar/>
+       <div className='container mt-4'>
+        {user?<Dashboard/>:<Login/>}
+       </div>
     </div>
   );
 }
+
+const Navbar = () =>{
+  const {user,logout} = useContext(UserContext);
+  return (
+    <h1>Navigation</h1>
+  );
+}
+
 
 export default App;
