947293ff0cdaf3b9ea1a72f22d214143d6649ab3
Ubuntu <labuser@ubuntu2204.jzx5afjpbpnupisvvtbsub54jd.rx.internal.cloudapp.net>
2024-07-03 12:12:08 +0000

    Nested

---
+++ Portfolio-website/nested-routing/src/Email.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Email = () => (
+    <div>
+        <h3>Email</h3>
+        <p>Why should I let you know my email</p>
+    </div>
+)
+
+export default Email;
---
+++ Portfolio-website/nested-routing/src/Phone.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Phone = () => (
+    <div>
+        <h3>Phone</h3>
+        <p>Why should I let you know my Phone</p>
+    </div>
+)
+
+export default Phone  ;
